f5adb96bfb5219caef6e485fb04f88516cc69a11
chadeau <chadeau@protonmail.ch>
2023-02-27 12:45:38 -0500

    +Docstrings: some cities have more data

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -163,7 +163,8 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users.
+    Data files for all cities do not contain all information"""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
